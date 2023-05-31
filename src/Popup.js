f14f5df70a30891c1b59bf00ae08467d5ab1c3ce
Seojoon Hwang <seojoonhwang@Seojoonui-MacBookPro.local>
2023-05-31 15:01:19 +0900

    팝업 버튼 코드 간결하게 수정

--- src/Popup.js
+++ src/Popup.js
@@ -6,15 +6,12 @@ export default function Popup() {
 
   return (
     <>
-      {popupVisible && (
-        <button onClick={() => setPopupVisible(false)}>팝업 닫기</button>
-      )}
-      {popupVisible || (
-        <button onClick={() => setPopupVisible(true)}>팝업 열기</button>
-      )}
+      <button onClick={() => setPopupVisible(!popupVisible)}>
+        팝업{popupVisible ? "닫기" : "열기"}
+      </button>
       <hr />
+      {`popupVisible : ${popupVisible}`}
       {popupVisible && <div style={{ width: 100, height: 100, border }}></div>}
-      <button className="btn text-xl">Button</button>
     </>
   );
 }
