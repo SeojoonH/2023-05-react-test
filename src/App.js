065f663cf8c9ecc1212373b445add3651a33fb80
Seojoon Hwang <seojoonhwang@Seojoonui-MacBookPro.local>
2023-06-07 14:00:44 +0900

    기존 배열 불러와 1초에 하나씩 표시되도록 설정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React, { useState } from "react";
 import NoRecord from "./NoRecord";
 
 function App() {
+  const [arr, setArr] = useState([1, 2, 3, 4, 5]);
+
+  setTimeout(() => {
+    setArr([...arr, arr.length + 1]);
+  }, 1000);
+
   return (
     <>
       <NoRecord />
+      {arr.join(",")}
     </>
   );
 }
