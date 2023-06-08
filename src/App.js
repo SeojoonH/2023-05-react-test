b5f67fad34826632435d9dc0e80ffc8c72fac645
Seojoon Hwang <seojoonhwang@Seojoonui-MacBookPro.local>
2023-06-08 15:42:29 +0900

    form 연습01

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import NoRecord from "./NoRecord";
 
 function App() {
+  const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+  const arr = nums.map((el, index) => <li key={index}>내용 {el}</li>);
   return (
     <>
       <NoRecord />
+
+      <hr />
+      <ul>{arr}</ul>
     </>
   );
 }
--- src/NoRecord.js
+++ src/NoRecord.js
@@ -1,25 +1,47 @@
 import React, { useState } from "react";
 
 export default function NoRecord() {
-  const [no, setNo] = useState(0);
   const [recordedNos, setRecordedNos] = useState([]);
 
-  const saveNo = () => {
-    setRecordedNos([...recordedNos, no]);
+  const saveNo = (form) => {
+    form.no.value = form.no.value.trim();
+
+    if (form.no.value.length === 0) {
+      // NaN가 나오는 것을 방지하기 위한 조건문
+      alert("숫자를 입력하세요.");
+      form.no.focus();
+      return;
+    }
+    setRecordedNos([...recordedNos, form.no.value]);
+    form.no.value = "";
+    form.no.focus = "";
   };
+
+  const li = recordedNos.map((el, index) => <li key={index}>{el}</li>);
 
   return (
     <>
-      <input
-        type="number"
-        value={no}
-        onChange={(e) => setNo(e.target.valueAsNumber)}
-      />
-      <button type="button" onClick={saveNo}>
-        기록
-      </button>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          saveNo(e.target);
+        }}
+      >
+        <input type="number" name="no" />
+        <button type="submit">기록</button>
+      </form>
       <hr />
+      <h1>기록된 숫자 v1</h1>
       기록된 숫자 : [{recordedNos.join(",")}]
+      <hr />
+      <h1>기록된 숫자 v2</h1>
+      <ul>{li}</ul>
+      <h1>기록된 숫자 v2-2</h1>
+      <ul>
+        {recordedNos.map((el, index) => (
+          <li key={index}>{el}</li>
+        ))}
+      </ul>
     </>
   );
 }
