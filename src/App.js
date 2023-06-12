4877c606073c840638761ce36469b8e7b397c8a5
Seojoon Hwang <seojoonhwang@Seojoonui-MacBookPro.local>
2023-06-12 16:59:49 +0900

    useEffect, useRef 사용 및 빈배열 추가해 한 번만 작동하도록 하기

--- src/App.js
+++ src/App.js
@@ -1,69 +1,41 @@
-import React, { useState } from "react";
-import classnames from "https://cdn.skypack.dev/classnames";
+import React, { useEffect, useState, useRef } from "react";
 
-function NotifyOnce({ children }) {
-  const [visible, setVisible] = useState(false);
-  const [workDone, setWorkDone] = useState(false);
-
-  if (workDone === false) {
-    setTimeout(function () {
-      setVisible(true);
-    }, 1000);
-
-    setTimeout(function () {
-      setVisible(false);
-    }, 3000);
-
-    setWorkDone(true);
-  }
-
-  return (
-    <div
-      className={classnames(
-        "fixed transition-all right-[10px]",
-        {
-          "top-[-60px]": !visible,
-        },
-        {
-          "top-[-10px]": visible,
-        }
-      )}
-    >
-      {children}
-    </div>
-  );
-}
-
-function Alert({ color: _color, children }) {
-  const color = _color ?? "white";
-  return (
-    <div className="alert alert-info">
-      <div className={`text-${color}`}>
-        <span>
-          <i className="fa-solid fa-circle-info"></i>
-        </span>
-        <span>{children}</span>
-      </div>
-    </div>
-  );
-}
-
-// const color = _color ?? "white";
-// 컬러가 있으면 컬러대로, 컬러가 없다면 white로.
+let AppCallCount = 0;
 
 function App() {
+  const inputNameRef = useRef(null);
+  const inputAgeRef = useRef(null);
+  const [no, setNo] = useState(0);
+
+  useEffect(() => {
+    inputNameRef.current.focus();
+  }, []);
+
   return (
     <>
-      <NotifyOnce>
-        <Alert>안녕하세요!</Alert>
-      </NotifyOnce>
-
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam,
-        tempora eligendi perferendis fuga rem cumque vel sed iure, est tempore
-        laborum molestiae repudiandae corrupti quidem! Quos alias illo quidem
-        veniam.
-      </div>
+      <input
+        ref={inputNameRef}
+        type="text"
+        placeholder="이름"
+        className="input input-bordered text text-white"
+      />
+      <hr />
+      <input
+        ref={inputAgeRef}
+        type="number"
+        placeholder="나이"
+        className="input input-bordered text text-white"
+      />
+      <hr />
+      <button
+        className="btn btn-outline"
+        onClick={() => {
+          setNo(no + 1);
+          inputAgeRef.current.focus();
+        }}
+      >
+        증가 : {no}
+      </button>
     </>
   );
 }
