eb9464a315141f759ffc4f0e5ff5f9fda61293ec
Seojoon Hwang <seojoonhwang@Seojoonui-MacBookPro.local>
2023-06-13 13:57:21 +0900

    소수점 구하는 법 및 시간 지연 방법

--- public/index.js
+++ public/index.js
@@ -1,1 +1,32 @@
-$(".btn-toggle-theme").click(() => $(":root").toggleClass("dark"));
+console.clear();
+
+function isPrimeNumber(no) {
+  for (let i = 2; i < no; i++) {
+    if (i * i > no) {
+      break;
+    }
+    if (no % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function getPrimeNumbers(max) {
+  const primeNumbers = [];
+
+  for (let i = 2; i <= max; i++) {
+    if (isPrimeNumber(i)) {
+      primeNumbers.push(i);
+    }
+  }
+  return primeNumbers;
+}
+
+function getPrimeNumbersCount(max) {
+  return getPrimeNumbers(max).length;
+}
+
+console.time("a");
+console.log(getPrimeNumbersCount(100000000));
+console.timeEnd("a");
